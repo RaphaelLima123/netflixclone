53ceb858f423f55614d9e294e7a489667c0e3a7e
Raphael Lima <raphaellimag@gmail.com>
2021-03-18 14:02:26 -0300

    Adiciona chamada async

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import getMovies from './services/tmdb';
 
 export default function App() {
+
+  useEffect(() => {
+    const load = async () => {
+      const req = await getMovies();
+      console.log('Passou por aqui', req);
+    }
+
+    load();
+  }, [])
+
   return (
     <h1>Projeto Netflix</h1>
   );
--- src/services/tmdb/index.js
+++ src/services/tmdb/index.js
@@ -1,3 +1,19 @@
 const API_KEY = '95cfdaa5a5bb42d215c7791c102ee168';
 const API_BASE = 'https://api.themoviedb.org/3';
 const language = 'language=pt-BR';
+
+const baseFetch = async (endpoint) => {
+  const firstFetch = await fetch(`${API_BASE}${endpoint}`);
+  const json = await firstFetch.json();
+  return json;
+}
+
+export default async function getMovies () {
+  return [
+    {
+      slug: 'originals',
+      title: 'Originais Netflix',
+      items: [await baseFetch(`/movie/550?api_key=${API_KEY}`)]
+    }
+  ];
+}
