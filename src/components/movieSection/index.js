964a91f30f5a11681e511396d02e6a27c90a6760
Raphael Lima <raphaellimag@gmail.com>
2021-03-19 13:25:34 -0300

    Adiciona poster para os filmes em Movie Section

--- src/components/movieSection/index.js
+++ src/components/movieSection/index.js
@@ -4,12 +4,17 @@ export default function MovieSection({title, items}){
 
   const url = `https://image.tmdb.org/t/p/w300`;
 
+  const firstResults = {};
+  items.map((item) => (
+    firstResults.items = item
+  ));
+  
   return(
       <div>
         <h2>{title}</h2>
-        <h3>{console.log("Aqui", title, items)}</h3>
+        {/* {<h3>{console.log("Aqui", title, secondResult)}</h3>} */}
         <div className="movieListArea">
-          {items.length > 0 && items.map((item, key) => (
+          {firstResults.items.results.length > 0 && firstResults.items.results.map((item, key) => (
             <img src={`${url}${item.poster_path}`} key={key}/>
           ))}
         </div>
